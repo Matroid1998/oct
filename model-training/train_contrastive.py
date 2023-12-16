6fa844e3fc73a8e0d493e08baae7d6eca5dcae0d
mehdi <mehdi.astaraki98@gmail.com>
2023-12-16 13:20:07 +0330

    compeleted

--- model-training/train_contrastive.py
+++ model-training/train_contrastive.py
@@ -5,6 +5,12 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 import os
+from tqdm import tqdm
+from datetime import datetime
+from torch.utils.tensorboard import SummaryWriter
+
+timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+tb_writer = SummaryWriter('OCT_trainer')
 train_data_paths = 'D:\octalldata'
 data_paths = [os.path.join(train_data_paths,file) for file in os.listdir(train_data_paths) if
               os.path.isfile(os.path.join(train_data_paths,file))]
@@ -28,29 +34,28 @@ num_epochs = 10
 for epoch in range(num_epochs):
     model.train()
     total_loss = 0
-    for batch in train_loader:
+    progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), desc=f"Epoch {epoch+1}/{num_epochs}")
+
+    for batch_idx, batch in progress_bar:
+        optimizer.zero_grad()
         positive1_images,positive2_images,*negative_images = batch
         # positive1_images,positive2_images = positive1_images.to(device),positive2_images.to(device)
-
-        # Concatenate all negative images into a single batch
         all_negatives = torch.cat(negative_images,dim = 0)
         # .to(device)
-
-        # Forward pass for positives and all negatives at once
         z_positive1 = model(positive1_images)
         z_positive2 = model(positive2_images)
         z_negatives = model(all_negatives)
-
-        # Reshape z_negatives back to list of tensors for each negative sample
-        z_negatives = z_negatives.split(positive1_images.size(0))
-
-        # Loss calculation
+        z_negatives = z_negatives.split(positive1_images.size(0)) ## final shape is (number of negatives, batch size)
         loss = loss_fn(z_positive1,z_positive2,z_negatives)
-        total_loss += loss.item()
-        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        print(loss)
-
+        total_loss += loss.item()
+        if batch_idx % 10 == 9:
+            last_loss = total_loss / 10 # loss per batch
+            print('  batch {} loss: {}'.format(batch_idx + 1, last_loss))
+            tb_x = epoch * len(train_loader) + batch_idx + 1
+            tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+            total_loss = 0
     avg_loss = total_loss / len(train_loader)
-    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss:.4f}")
+    tb_writer.add_scalar('Epoch/Average Loss', avg_loss, epoch)
+    print(f"Epoch Completed: {epoch+1}/{num_epochs}, Average Loss: {avg_loss:.4f}")
